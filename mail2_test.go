302d834255624c156e79a055bce87de94665e765
Michel Casabianca <casa@sweetohm.net>
2017-04-21 11:03:38 +0200

    Fixed test

--- mail2_test.go
+++ mail2_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSendMail(t *testing.T) {
 	err := SendMail("smtp.orange.fr:25", "michel.casabianca@gmail.com",
-		"michel.casabianca@gmail.com", "Test", "This is a test!", "mail2.go")
+		"michel.casabianca@gmail.com", "", "", "Test", "This is a test!", "mail2.go", "", "", false)
 	if err != nil {
 		t.Error("Sending email")
 	}
