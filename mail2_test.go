1093b41de6b57ef60290db729f1f37d7750c8a36
Michel Casabianca <casa@sweetohm.net>
2020-02-11 23:40:41 +0100

    Fmted

--- mail2_test.go
+++ mail2_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestSendMail(t *testing.T) {
 	err := SendMail(os.Getenv("MAIL_SMTP"),
-					os.Getenv("MAIL_RECIPIENT"),
-					os.Getenv("MAIL_RECIPIENT"),
-					"", "", "Test", "This is a test!", "mail2.go",
-					os.Getenv("MAIL_USERNAME"),
-					os.Getenv("MAIL_PASSWORD"),
-					false)
+		os.Getenv("MAIL_RECIPIENT"),
+		os.Getenv("MAIL_RECIPIENT"),
+		"", "", "Test", "This is a test!", "mail2.go",
+		os.Getenv("MAIL_USERNAME"),
+		os.Getenv("MAIL_PASSWORD"),
+		false)
 	if err != nil {
 		t.Fatalf("Error sending email: %v", err)
 	}
